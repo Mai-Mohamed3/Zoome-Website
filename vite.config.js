193bdd67c5cfba7857b95261cf0392b6d1231d94
Mai-Mohamed3 <mai.mo.alii3@gmail.com>
2026-04-25 16:11:23 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// بدلي `your-repo-name` باسم الريبو بتاعك على GitHub
 export default defineConfig({
-  base: '/',
+  base: '/Zoome-Website/',
   plugins: [react()],
 })
